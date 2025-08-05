d4971fc040618758317e51cf8750b0231e9dda00
ABDELILAH <134460830+ELKARMOUDI@users.noreply.github.com>
2025-08-05 21:27:15 +0900

    App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Box, useColorModeValue } from "@chakra-ui/react";
 import { Route, Routes } from "react-router-dom";
 
-import Homepage from "./pages/Homepage";
+import Homepage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import Navbar from "./components/Navbar";
 
